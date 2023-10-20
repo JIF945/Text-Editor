2a93455f556ded0f942f7572beeafbfb05e2a702
James <joshua.james918@gmail.com>
2023-10-20 14:19:46 -0500

    create connection, new transaction, desired object, add method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,41 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('textEditor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('textEditor')) {
+        console.log('textEditor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('textEditor', { keyPath: 'id', autoIncrement: true });
+      console.log('textEditor database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.log('putDb not implemented');
+
+ // Create a connection to the database database and version we want to use.
+ const textEditorDb = await openDB('textEditor', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = textEditorDb.transaction('textEditor', 'readwrite');
+
+ // Open up the desired object store.
+ const store = tx.objectStore('textEditor');
+
+// Use the .add() method on the store and pass in the content.
+const request = store.add({ id: 1, value: content });
+
+const result = await request;
+console.log('Data saved to the database', result);
+return result;
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
